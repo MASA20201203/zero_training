8d42ccacb45b1831400b683abd90d3162e250019
masa <koedame032@gmail.com>
2021-01-25 15:28:18 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test 'should root' do
+    get root_url
+    assert_response :success
+    assert_select 'title', 'Home | 0tore'
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', 'Home | 0tore'
   end
 
   test 'should get help' do
     get static_pages_help_url
     assert_response :success
+    assert_select 'title', 'Help | 0tore'
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
+    assert_select 'title', 'About | 0tore'
   end
 end
