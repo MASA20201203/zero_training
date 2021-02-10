bf02540a9fef1bce32220bd49a8b0fe0ee44a46e
masa <koedame032@gmail.com>
2021-02-10 13:47:32 +0900

    ヘッダーのHelp、Aboutを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get 'static_pages/about'
   get '/courses', to: 'static_pages#courses'
   get '/login', to: 'static_pages#login'
   get '/signup', to: 'static_pages#signup'
